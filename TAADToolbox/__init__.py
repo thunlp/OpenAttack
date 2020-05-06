a03d40670376bbee9a10fee6413d5ebccf7bae11
a710128 <qbjzgy@sina.com>
2020-05-06 20:00:46 +0800

    add comment for TAADToolbox/__init__.py

--- TAADToolbox/__init__.py
+++ TAADToolbox/__init__.py
@@ -1,4 +1,21 @@
+# data manager
 from .data_manager import DataManager
+# attacker
+from .attacker import Attacker
 from . import attackers
+# classifier
 from .classifier import Classifier
+# from .classifiers import PytorchClassifier, TensorflowClassifier
+from . import classifiers
+# attacker_eval
+# from .attacker_evals import DefaultAttackerEval
+from . import attacker_evals
+from .attacker_eval import AttackerEval
+# exception
+from . import exceptions
+from .exception import AttackException
+# substitute
+from .substitute import Substitute
+from . import substitutes 
+# utils
 from . import utils
