f2f2f68d12672a5cec5c9961ee0051ec79b36455
a710128 <qbjzgy@sina.com>
2020-05-06 19:43:12 +0800

    fixed a bug in test script

--- test/run_test.py
+++ test/run_test.py
@@ -11,7 +11,7 @@ def main(args,):
     test_cases = unittest.defaultTestLoader.discover(TEST_BASE, pattern="test_*.py",)
     runner = unittest.runner.TextTestRunner(verbosity=args.verbosity)
     ret = runner.run(test_cases)
-    sys.exit(len(ret.failures))
+    sys.exit(len(ret.failures) + len(ret.errors))
 
 
 if __name__ == "__main__":
