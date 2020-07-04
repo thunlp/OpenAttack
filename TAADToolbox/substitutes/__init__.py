7dd2e01354330033ec7bd7f7e82a7a333a395b68
unknown <zhangtingji18@163.com>
2020-07-04 13:02:16 +0800

    attackers and sunstitutes

--- TAADToolbox/substitutes/__init__.py
+++ TAADToolbox/substitutes/__init__.py
@@ -1,2 +1,6 @@
 from .embedbase import EmbedBasedSubstitute
 from .counter_fit import CounterFittedSubstitute
+from .dces import DcesSubstitute
+from .eces import EcesSubstitute
+from .hownet import HowNetSubstitute
+from .wordnet import WordNetSubstitute
--- TAADToolbox/substitutes/dces.py
+++ TAADToolbox/substitutes/dces.py
@@ -1,19 +1,17 @@
-'''
+"""
     find the nearest neighbours in the list of unicode characters by finding characters
     with the largest number of matching tokens in the text description.
     根据unicode description找到描述相似的字符
     __call__函数参数：原字符，相似字符数量
     返回值：（字符，prob）列表，prob基本为均分，和为1
-    进一步工作：对一段文本以概率p扰动
-'''
+    require:
+    DataManager.download("DCES")
+"""
 
 from ..substitute import Substitute
+from ..data_manager import DataManager
 # from ..exceptions import
-import pandas as pd
 import numpy as np
-import random
-from sklearn.neighbors import NearestNeighbors
-from sklearn.feature_extraction.text import CountVectorizer
 
 
 disallowed = ['TAG', 'MALAYALAM', 'BAMUM', 'HIRAGANA', 'RUNIC', 'TAI', 'SUNDANESE', 'BATAK', 'LEPCHA', 'CHAM',
@@ -34,17 +32,8 @@ def get_hex_string(ch):
 class DcesSubstitute(Substitute):
 
     def __init__(self):
-        self.descs = pd.read_csv('NamesList.txt', skiprows=np.arange(16), header=None, names=['code', 'description'],
-                                 delimiter='\t')
-        self.descs = self.descs.dropna(0)
-        self.descs_arr = self.descs.values
-        self.vectorizer = CountVectorizer(max_features=1000)
-        self.desc_vecs = self.vectorizer.fit_transform(self.descs_arr[:, 0]).astype(float)
-        self.vecsize = self.desc_vecs.shape[1]
-        self.vec_colnames = np.arange(self.vecsize)
-        self.desc_vecs = pd.DataFrame(self.desc_vecs.todense(), index=self.descs.index, columns=self.vec_colnames)
-        self.descs = pd.concat([self.descs, self.desc_vecs], axis=1)
-        # 预处理，准备好所有字符的描述
+        self.vec_colnames, self.descs, self.neigh = DataManager.load("DCES")
+        # load
 
     def __call__(self, char, threshold):  # 原字符，topn
 
@@ -55,12 +44,11 @@ class DcesSubstitute(Substitute):
             description = self.descs['description'][self.descs['code'] == c].values[0]
         else:
             # raise exception
-            print("failed to disturb %s" % char)
-            return char, np.array([])
+            # print("failed to disturb %s" % char)
+            return [char, 1]
         # 找不到字符
 
-        tokens = description.split(' ')  # 描述
-        # print(tokens)
+        tokens = description.split(' ')
         case = 'unknown'
         identifiers = []
 
@@ -96,24 +84,22 @@ class DcesSubstitute(Substitute):
         matches = np.array(matches)  # 找到所有共同token的字符
 
         if not len(matches):
-            print("cannot disturb, find no match")
-            return [], []  # cannot disturb this one
+            # print("cannot disturb, find no match")
+            return [(char, 1)]  # cannot disturb this one
 
         match_vecs = self.descs[self.vec_colnames].loc[matches]  # description
 
-        neigh = NearestNeighbors(metric='euclidean')  # nearest neighbors
         Y = match_vecs.values
-        neigh.fit(Y)
+        self.neigh.fit(Y)
 
         X = self.descs[self.vec_colnames].values[self.descs['code'] == c]
 
         if Y.shape[0] > threshold:
-            dists, idxs = neigh.kneighbors(X, threshold, return_distance=True)
+            dists, idxs = self.neigh.kneighbors(X, threshold, return_distance=True)
         else:
-            dists, idxs = neigh.kneighbors(X, Y.shape[0], return_distance=True)
+            dists, idxs = self.neigh.kneighbors(X, Y.shape[0], return_distance=True)
 
         # turn distances to some heuristic probabilities
-        # print(dists.flatten())
         probs = np.exp(-0.5 * dists.flatten())
         probs = probs / np.sum(probs)
 
@@ -121,17 +107,13 @@ class DcesSubstitute(Substitute):
         # print(idxs.flatten())
         charcodes = self.descs['code'][matches[idxs.flatten()]]
 
-        # print(charcodes.values.flatten())
-
         chars = []
         for charcode in charcodes:
             chars.append(chr(int(charcode, 16)))
-        print(chars)
+        # print(chars)
         ret = []
         for i in range(len(chars)):
             ret.append((chars[i], probs[i]))  # 以概率
         # for char in chars:
         #    ret.append((char, 1))
         return ret
-
-    # def disturb_sentence(self, sentence, prob):  # 以概率prob对一句话进行替换
