480395c7f3bf5e447dfb29e3d0b8a887b5bbfa3b
unknown <zhangtingji18@163.com>
2020-07-07 11:10:50 +0800

    7_7

---
+++ TAADToolbox/attackers/viper.py
@@ -0,0 +1,51 @@
+from ..attacker import Attacker
+from ..substitutes import DcesSubstitute
+from ..substitutes import EcesSubstitute
+import numpy as np
+import random
+
+
+DEFAULT_CONFIG = {
+    "prob": 0.3,
+    "topn": 12
+}
+
+
+class ViperAttacker(Attacker):
+    def __init__(self, **kwargs):
+        self.config = DEFAULT_CONFIG.copy()
+        self.config.update(kwargs)
+        self.dces = DcesSubstitute()
+        self.eces = EcesSubstitute()
+        self.mydict = {}
+        self.topn = self.config["topn"]
+        self.prob = self.config["prob"]
+
+    def __call__(self, clsf, x_orig, target=None):
+        """
+        * **clsf** : **Classifier** .
+        * **x_orig** : Input sentence.
+        """
+        a = x_orig.rstrip("\n")
+        out = []
+        for c in a:
+            if c not in self.mydict:
+                similar_chars, probs = [], []
+                dces_list = self.dces.__call__(c, self.topn)
+                for sc, pr in dces_list:
+                    similar_chars.append(sc)
+                    probs.append(pr)
+                probs = probs[:len(similar_chars)]
+                probs = probs / np.sum(probs)
+                self.mydict[c] = (similar_chars, probs)
+            else:
+                similar_chars, probs = self.mydict[c]
+
+            r = random.random()
+            if r < self.prob and len(similar_chars):
+                s = np.random.choice(similar_chars, 1, replace=True, p=probs)[0]
+            else:
+                s = c
+            out.append(s)
+        ans = "".join(out)
+        return ans, clsf.get_pred([ans])[0]
