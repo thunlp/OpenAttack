08bfc0809c077c28104fa0e383522b9e822dbc7c
a710128 <qbjzgy@sina.com>
2020-07-15 08:46:35 +0800

    upd sea

--- OpenAttack/attackers/sea/__init__.py
+++ OpenAttack/attackers/sea/__init__.py
@@ -12,9 +12,13 @@ DEFAULT_BACK_PATHS = ['french_english_model_acc_68.51_ppl_4.43_e13.pt', 'portugu
 
 DEFAULT_CONFIG = {
     "processor": DefaultTextProcessor(),
+    "rules": None,
+}
+
+TRAIN_CONFIG = {
+    "processor": DefaultTextProcessor(),
     "gpu_id": 0,
     "cuda": True,
-    "clsf": None,
     "topk": 200, 
     "threshold": -15,
     "min_freq": 0.005,
@@ -44,13 +48,12 @@ class SEAAttacker(Attacker):
         self.config = DEFAULT_CONFIG.copy()
         self.config.update(kwargs)
         check_parameters(DEFAULT_CONFIG, self.config)
-        model_path = DataManager.load("TranslationModels")
-
-        to_paths = [model_path[x] for x in DEFAULT_TO_PATHS]
-        back_paths = [model_path[x] for x in DEFAULT_BACK_PATHS]
-        self.ps = self.paraphrase_scorer.ParaphraseScorer(to_paths=to_paths, back_paths=back_paths, gpu_id=self.config["gpu_id"], cuda=self.config["cuda"])
         self.tokenizer = self.replace_rules.Tokenizer(self.config["processor"])
-        
+        self.x = self.config["rules"][0]
+        self.really_frequent_rules = self.config["rules"][1]
+        self.frequent_rules = self.config["rules"][2]
+        self.rule_flips = self.config["rules"][3]
+        self.token_right = self.config["rules"][4]
 
 
 
@@ -67,57 +70,67 @@ class SEAAttacker(Attacker):
         x_orig = self.tokenizer.tokenize(x_orig)[0]
         for r in self.x:
             rid = self.really_frequent_rules[r]
-            rule =  self.frequent_rules[rid]
+            rule = self.frequent_rules[rid]
             for f in self.rule_flips[rid][:2]:
                 if self.token_right[f] != x_orig:
                     continue
                 new = rule.apply(self.token_right[f])[1]
                 ans = clsf.get_pred([new])[0]
-                print(new)
                 if targeted:
                     if ans == target:
                         return new, ans
                 else:
                     if ans != target:
                         return new, ans
+        
         return None
 
+    @classmethod
+    def get_rules(cls, clsf, sentence_list, **kwargs):
+        from . import paraphrase_scorer
+        from . import onmt_model
+        from . import replace_rules
+        from . import rule_picking
 
+        def find_flips(instance, clsf, ps, tokenizer, topk=10, threshold=-10):
+            orig_pred = clsf.get_pred([instance])[0]
+            instance_for_onmt = onmt_model.clean_text(instance, only_upper=False)
+            paraphrases = ps.generate_paraphrases(instance_for_onmt, topk=topk, edit_distance_cutoff=5, threshold=threshold)
+            texts = tokenizer.clean_for_model(tokenizer.clean_for_humans([x[0] for x in paraphrases]))
+            preds = clsf.get_pred(texts)
 
-    def find_flips(self, instance, clsf, topk=10, threshold=-10):
-        orig_pred = clsf.get_pred([instance])[0]
-        instance_for_onmt = self.onmt_model.clean_text(instance, only_upper=False)
-        paraphrases = self.ps.generate_paraphrases(instance_for_onmt, topk=topk, edit_distance_cutoff=5, threshold=threshold)
-        texts = self.tokenizer.clean_for_model(self.tokenizer.clean_for_humans([x[0] for x in paraphrases]))
-        preds = clsf.get_pred(texts)
+            fs = [(texts[i], paraphrases[i][1]) for i in np.where(preds != orig_pred)[0]]
+            return fs
 
-        fs = [(texts[i], paraphrases[i][1]) for i in np.where(preds != orig_pred)[0]]
-        return fs
+        config = TRAIN_CONFIG.copy()
+        config.update(kwargs)
+        check_parameters(TRAIN_CONFIG.keys(), config)
 
-    def get_rules(self, clsf, sentence_list):
-        val_for_onmt = [self.onmt_model.clean_text(sentence.lower(), only_upper=False) for sentence in sentence_list]
+        model_path = DataManager.load("TranslationModels")
+
+        to_paths = [model_path[x] for x in DEFAULT_TO_PATHS]
+        back_paths = [model_path[x] for x in DEFAULT_BACK_PATHS]
+        ps = paraphrase_scorer.ParaphraseScorer(to_paths=to_paths, back_paths=back_paths, gpu_id=config["gpu_id"], cuda=config["cuda"])
+        tokenizer = replace_rules.Tokenizer(config["processor"])
+
+        val_for_onmt = [onmt_model.clean_text(sentence.lower(), only_upper=False) for sentence in sentence_list]
 
         orig_scores = {}
         flips = collections.defaultdict(lambda: [])
         right_val = val_for_onmt
-        self.right_preds = clsf.get_pred(right_val)
+        right_preds = clsf.get_pred(right_val)
 
         for i, sentence in enumerate(right_val):
-            print(sentence)
             if sentence in flips:
                 continue
-            fs = self.find_flips(sentence, clsf, topk=self.config["topk"], threshold=self.config["threshold"])
+            fs = find_flips(sentence, clsf, ps, tokenizer, topk=config["topk"], threshold=config["threshold"])
             flips[sentence].extend([x[0] for x in fs])
-            print(len(flips))
 
-
-
-        tr2 = self.replace_rules.TextToReplaceRules(self.config["processor"], right_val, [], 
-                                                    min_freq=self.config["min_freq"], min_flip=0.00, ngram_size=self.config["ngram_size"])
-        self.frequent_rules = []
+        tr2 = replace_rules.TextToReplaceRules(config["processor"], right_val, [], 
+                                                    min_freq=config["min_freq"], min_flip=0.00, ngram_size=config["ngram_size"])
+        frequent_rules = []
         rule_idx = {}
-        self.rule_flips = {}
-        print("find rules")
+        rule_flips = {}
         for z, f in enumerate(flips):
             rules = tr2.compute_rules(f, flips[f], use_pos=True, use_tags=False)
             for rs in rules:
@@ -125,26 +138,26 @@ class SEAAttacker(Attacker):
                     if r.hash() not in rule_idx:
                         i = len(rule_idx)
                         rule_idx[r.hash()] = i
-                        self.rule_flips[i] = []
-                        self.frequent_rules.append(r)
+                        rule_flips[i] = []
+                        frequent_rules.append(r)
                     i = rule_idx[r.hash()]
-                    self.rule_flips[i].append(z)
+                    rule_flips[i].append(z)
 
-        self.token_right = self.tokenizer.tokenize(right_val)
+        token_right = tokenizer.tokenize(right_val)
         model_preds = {}
 
 
-        self.rule_flips = {}
+        rule_flips = {}
         rule_other_texts = {}
         rule_other_flips = {}
         rule_applies = {}
-        for i, r in enumerate(self.frequent_rules):
+        for i, r in enumerate(frequent_rules):
             idxs = list(tr2.get_rule_idxs(r))
-            to_apply = [self.token_right[x] for x in idxs]
+            to_apply = [token_right[x] for x in idxs]
             applies, nt = r.apply_to_texts(to_apply, fix_apostrophe=False)
             applies = [idxs[x] for x in applies]
 
-            old_labels = self.right_preds[applies]
+            old_labels = right_preds[applies]
             to_compute = [x for x in nt if x not in model_preds]
             if to_compute:
                 preds = clsf.get_pred(to_compute)
@@ -153,22 +166,22 @@ class SEAAttacker(Attacker):
             new_labels = np.array([model_preds[x] for x in nt])
             where_flipped = np.where(new_labels != old_labels)[0]
             flips = sorted([applies[x] for x in where_flipped])
-            self.rule_flips[i] = flips
+            rule_flips[i] = flips
             rule_other_texts[i] = nt
             rule_other_flips[i] = where_flipped
             rule_applies[i] = applies
-        self.really_frequent_rules = [i for i in range(len(self.rule_flips)) if len(self.rule_flips[i]) > 1]
+        really_frequent_rules = [i for i in range(len(rule_flips)) if len(rule_flips[i]) > 1]
 
 
         threshold = -7.15
         orig_scores = {}
         for i, t in enumerate(right_val):
-            orig_scores[i] = self.ps.score_sentences(t, [t])[0]
+            orig_scores[i] = ps.score_sentences(t, [t])[0]
         ps_scores = {}
-        self.ps.last = None
+        ps.last = None
         rule_scores = []
         rejected = set()
-        for idx, i in enumerate(self.really_frequent_rules):
+        for idx, i in enumerate(really_frequent_rules):
             orig_texts =  [right_val[z] for z in rule_applies[i]]
             orig_scor = [orig_scores[z] for z in rule_applies[i]]
             scores = np.ones(len(orig_texts)) * -50
@@ -183,7 +196,7 @@ class SEAAttacker(Attacker):
                     if n == '':
                         score = -40
                     else:
-                        score = self.ps.score_sentences(o, [n])[0]
+                        score = ps.score_sentences(o, [n])[0]
                     ps_scores[o][n] = min(0, score - orig)
                 scores[j] = ps_scores[o][n]
                 if ps_scores[o][n] < threshold:
@@ -192,22 +205,23 @@ class SEAAttacker(Attacker):
                     rejected.add(idx)
                     break
             rule_scores.append(scores)
-        rule_flip_scores = [rule_scores[i][rule_other_flips[self.really_frequent_rules[i]]] for i in range(len(rule_scores))]
-        frequent_flips = [np.array(rule_applies[i])[rule_other_flips[i]] for i in self.really_frequent_rules]
-        rule_precsupports = [len(rule_applies[i]) for i in self.really_frequent_rules]
+        rule_flip_scores = [rule_scores[i][rule_other_flips[really_frequent_rules[i]]] for i in range(len(rule_scores))]
+        frequent_flips = [np.array(rule_applies[i])[rule_other_flips[i]] for i in really_frequent_rules]
+        rule_precsupports = [len(rule_applies[i]) for i in really_frequent_rules]
         threshold=-7.15
 
-        disqualified = self.rule_picking.disqualify_rules(rule_scores, frequent_flips,
+        disqualified = rule_picking.disqualify_rules(rule_scores, frequent_flips,
                           rule_precsupports, 
                       min_precision=0.0, min_flips=6, 
                          min_bad_score=threshold, max_bad_proportion=.10,
                           max_bad_sum=999999)
 
-        self.x = self.rule_picking.choose_rules_coverage(rule_flip_scores, frequent_flips, None,
-                          None, len(self.right_preds),
+        x = rule_picking.choose_rules_coverage(rule_flip_scores, frequent_flips, None,
+                          None, len(right_preds),
                                 frequent_scores_on_all=None, k=10, metric='max',
                       min_precision=0.0, min_flips=0, exp=True,
                          min_bad_score=threshold, max_bad_proportion=.1,
                           max_bad_sum=999999,
                          disqualified=disqualified,
                          start_from=[])
+        return (x, really_frequent_rules, frequent_rules, rule_flips, token_right)
