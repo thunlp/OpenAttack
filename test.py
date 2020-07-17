1078b64312023960dfb595953636a22deeb94a28
a710128 <qbjzgy@sina.com>
2020-07-17 13:38:20 +0800

    remove test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import OpenAttack
-
-clsf = OpenAttack.DataManager.load("Victim.BiLSTM.SST")
-sea = OpenAttack.attackers.SEAAttacker()
-dataset = OpenAttack.DataManager.load("Dataset.SST.sample")[:10]
-
-sea.get_rules(clsf, dataset)
