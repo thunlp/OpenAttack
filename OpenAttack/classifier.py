c55c092c413fcfc2bc9a74834cf4191fefd721b9
a710128 <qbjzgy@sina.com>
2020-07-31 12:59:28 +0800

    update classifier supports meta

--- OpenAttack/classifier.py
+++ OpenAttack/classifier.py
@@ -6,9 +6,10 @@ class Classifier(metaclass=abc.ABCMeta):
     def __init__(self, *args, **kwargs):
         pass
 
-    def get_pred(self, input_, meta={}):
+    def get_pred(self, input_, meta):
         """
         :param list input_: A list of sentences which we want to predict.
+        :param dict meta: Meta of DataInstance (**Optional parameter**).
         :return: The result is a 1-dim numpy.array, *[pred1, pred2, ...]*.
         :rtype: numpy.array(dtype=numpy.long)
 
@@ -16,9 +17,10 @@ class Classifier(metaclass=abc.ABCMeta):
         """
         return self.get_prob(input_, meta).argmax(axis=1)
 
-    def get_prob(self, input_, meta={}):
+    def get_prob(self, input_, meta):
         """
         :param list input_: A list of sentences of which we want to get the probabilities.
+        :param dict meta: Meta of DataInstance (**Optional parameter**).
         :return: The result is a 2-dim numpy.array. The first dimension represents sentences and the second dimension represents probabilities of every type.
         :rtype: numpy.array(dtype=numpy.float)
 
@@ -29,10 +31,11 @@ class Classifier(metaclass=abc.ABCMeta):
         x_batch = [ list(map(lambda x:x[0], processor.get_tokens(sent))) for sent in input_ ]
         return self.get_grad(x_batch, [0] * len(x_batch), meta)[0]
 
-    def get_grad(self, input_, labels):
+    def get_grad(self, input_, labels, meta):
         """
         :param list input_: A list of lists of tokens of which we want to get the gradients.
         :param list labels: A list of types based on which we want to get the gradients.
+        :param dict meta: Meta of DataInstance (**Optional parameter**).
         :return: The result is a tuple of 2 numpy.array. The first numpy.array is same as the return of get_prob and the second numpy.array is gradients on the model's input, not the input\_. So the shape of the second numpy.array is the same model input.
         :rtype: tuple
 
@@ -50,9 +53,11 @@ class Classifier(metaclass=abc.ABCMeta):
             raise TypeError("Classifier.%s missing %d required positional argument" % (name, reqlen - len(args)))
         if not isinstance(args[0], list):
             raise TypeError("input_ should be a list of %s" % ("tokens" if name == "get_grad" else "str"))
-        print(fuclen, reqlen)
         if fuclen == reqlen:
             args = args[:reqlen]
+        elif len(args) < fuclen:
+            args = tuple([*args] + [{}])
+
         return func(*args, **kwargs)
 
     
