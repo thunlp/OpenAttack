f96dc4734c49c0b442cf0737756eab313b7a60c5
a710128 <qbjzgy@sina.com>
2020-07-31 11:09:44 +0800

    update classifier

--- OpenAttack/classifier.py
+++ OpenAttack/classifier.py
@@ -1,11 +1,12 @@
 import abc
+import functools
 from .exceptions import ClassifierNotSupportException
 
 class Classifier(metaclass=abc.ABCMeta):
     def __init__(self, *args, **kwargs):
         pass
 
-    def get_pred(self, input_):
+    def get_pred(self, input_, meta={}):
         """
         :param list input_: A list of sentences which we want to predict.
         :return: The result is a 1-dim numpy.array, *[pred1, pred2, ...]*.
@@ -13,9 +14,9 @@ class Classifier(metaclass=abc.ABCMeta):
 
         This is used to get the predictions of a batch of sentences.
         """
-        return self.get_prob(input_).argmax(axis=1)
+        return self.get_prob(input_, meta).argmax(axis=1)
 
-    def get_prob(self, input_):
+    def get_prob(self, input_, meta={}):
         """
         :param list input_: A list of sentences of which we want to get the probabilities.
         :return: The result is a 2-dim numpy.array. The first dimension represents sentences and the second dimension represents probabilities of every type.
@@ -26,7 +27,7 @@ class Classifier(metaclass=abc.ABCMeta):
         from .text_processors import DefaultTextProcessor
         processor = DefaultTextProcessor()
         x_batch = [ list(map(lambda x:x[0], processor.get_tokens(sent))) for sent in input_ ]
-        return self.get_grad(x_batch, [0] * len(x_batch))[0]
+        return self.get_grad(x_batch, [0] * len(x_batch), meta)[0]
 
     def get_grad(self, input_, labels):
         """
@@ -38,14 +39,26 @@ class Classifier(metaclass=abc.ABCMeta):
         This is used to get the gradients of a batch of sentences(presented as lists of tokens) on the appointed types.
         """
         raise ClassifierNotSupportException()
+
+
+    def __hook(self, func, name, *args, **kwargs):
+        fuclen = func.__code__.co_argcount - 1
+        reqlen = 2 if name == "get_grad" else 1
+        if len(args) > reqlen + 1:
+            raise TypeError("Classifier.%s get too many arguments" % name)
+        if len(args) < reqlen:
+            raise TypeError("Classifier.%s missing %d required positional argument" % (name, reqlen - len(args)))
+        if not isinstance(args[0], list):
+            raise TypeError("input_ should be a list of %s" % ("tokens" if name == "get_grad" else "str"))
+        print(fuclen, reqlen)
+        if fuclen == reqlen:
+            args = args[:reqlen]
+        return func(*args, **kwargs)
+
     
     def __getattribute__(self, name):
         ret = super().__getattribute__(name)
         if name in ["get_pred", "get_prob", "get_grad"]:
-            def checker(*args):
-                if not isinstance(args[0], list):
-                    raise TypeError("input_ should be a list of str")
-                return ret(*args)
-            return checker
+            return functools.partial(self.__hook, ret, name)
         return ret
 
