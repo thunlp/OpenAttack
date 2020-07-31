94fcfdc0fff5977f872c8a4c1dbc7cddf97a99d5
a710128 <qbjzgy@sina.com>
2020-07-31 15:18:48 +0800

    rewrite example 4

--- examples/custom_measurement.py
+++ examples/custom_measurement.py
@@ -1,17 +1,19 @@
 import OpenAttack
 from nltk.translate.bleu_score import sentence_bleu
 
-class CustomAttackEval(OpenAttack.attack_evals.DefaultAttackEval):
+class CustomAttackEval(OpenAttack.DefaultAttackEval):
     def __init__(self, attacker, clsf, processor=OpenAttack.DefaultTextProcessor(), **kwargs):
         super().__init__(attacker, clsf, processor=processor, **kwargs)
         self.__processor = processor
-        
+    
+    
     def measure(self, x_orig, x_adv):
         info = super().measure(x_orig, x_adv)
 
-        token_orig = [token for token, pos in self.__processor.get_tokens(x_orig)]
-        token_adv = [token for token, pos in self.__processor.get_tokens(x_adv)]
-        info["Bleu"] = sentence_bleu([x_orig], x_adv)
+        if info["Succeed"]:
+            token_orig = [token for token, pos in self.__processor.get_tokens(x_orig)]
+            token_adv = [token for token, pos in self.__processor.get_tokens(x_adv)]
+            info["Bleu"] = sentence_bleu([x_orig], x_adv)
 
         return info
     
@@ -20,7 +22,6 @@ class CustomAttackEval(OpenAttack.attack_evals.DefaultAttackEval):
         if info["Succeed"]:
             self.__result["bleu"] += info["Bleu"]
         return info
-
     
     def clear(self):
         super().clear()
@@ -30,12 +31,13 @@ class CustomAttackEval(OpenAttack.attack_evals.DefaultAttackEval):
         result = super().get_result()
         result["Avg. Bleu"] = self.__result["bleu"] / result["Successful Instances"]
         return result
+    
 
 def main():
     clsf = OpenAttack.load("Victim.BiLSTM.SST")
     dataset = OpenAttack.load("Dataset.SST.sample")[:10]
 
-    attacker = OpenAttack.attackers.PWWSAttacker()
+    attacker = OpenAttack.attackers.GeneticAttacker()
     attack_eval = CustomAttackEval(attacker, clsf)
     attack_eval.eval(dataset, visualize=True)
 
