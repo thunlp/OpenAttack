3a702157486b5a7fc600d3bbac41d8521ea503d7
a710128 <qbjzgy@sina.com>
2020-08-08 16:39:52 +0800

    fixed nltk load on Windows #97

--- OpenAttack/data/nltk_perceptron_pos_tagger.py
+++ OpenAttack/data/nltk_perceptron_pos_tagger.py
@@ -16,5 +16,5 @@ DOWNLOAD = make_zip_downloader(URL, "averaged_perceptron_tagger.pickle")
 
 def LOAD(path):
     ret = __import__("nltk").tag.PerceptronTagger(load=False)
-    ret.load(os.path.join(path, "averaged_perceptron_tagger.pickle"))
+    ret.load("file:" + os.path.join(path, "averaged_perceptron_tagger.pickle"))
     return ret.tag
