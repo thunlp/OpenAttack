c5adfcec8234c3edb7d4c1aa1a3de3ac6802e6a0
zgy <qbjooo@qq.com>
2020-09-23 13:17:40 +0800

    move tests

---
+++ slow_tests/attackers.py
@@ -0,0 +1,23 @@
+import OpenAttack
+def get_attackers(dataset, clsf):
+
+    rules = OpenAttack.attackers.SEAAttacker.get_rules(clsf, dataset)
+    triggers = OpenAttack.attackers.UATAttacker.get_triggers(clsf, dataset, word2id=clsf.word2id, embedding=clsf.embedding)
+
+
+    attackers = [
+        OpenAttack.attackers.FDAttacker(word2id=clsf.config["word2id"], embedding=clsf.config["embedding"]),
+        OpenAttack.attackers.SEAAttacker(rules=rules),
+        OpenAttack.attackers.UATAttacker(triggers=triggers),
+        OpenAttack.attackers.TextBuggerAttacker(),
+        OpenAttack.attackers.TextFoolerAttacker(),
+        OpenAttack.attackers.VIPERAttacker(),
+        OpenAttack.attackers.DeepWordBugAttacker(),
+        OpenAttack.attackers.GANAttacker(),
+        OpenAttack.attackers.GeneticAttacker(),
+        OpenAttack.attackers.HotFlipAttacker(),
+        OpenAttack.attackers.PWWSAttacker(),
+        OpenAttack.attackers.SCPNAttacker(),
+        OpenAttack.attackers.PSOAttacker(),
+    ]
+    return attackers
