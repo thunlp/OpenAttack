ce95e640d8db9365a6bec671e4c9544614f61a98
zgy <qbjooo@qq.com>
2020-09-23 13:03:33 +0800

    tensorflow allow growth

--- OpenAttack/utils/tf_fix.py
+++ OpenAttack/utils/tf_fix.py
@@ -9,4 +9,8 @@ def init_tensorflow():
     import logging
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
     tf.get_logger().setLevel(logging.ERROR)
+
+    gpus = tf.config.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
     return
