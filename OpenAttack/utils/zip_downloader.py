44de541fb3cce46430dc0de4ddbc66c534edec49
zgy <qbjooo@qq.com>
2020-09-23 10:38:14 +0800

    Fixed OverflowException while downloading large file

--- OpenAttack/utils/zip_downloader.py
+++ OpenAttack/utils/zip_downloader.py
@@ -12,14 +12,29 @@ def make_zip_downloader(URL, file_list=None):
         file_list = [file_list]
 
     def DOWNLOAD(path):
-        with urllib.request.urlopen(URL) as f:
-            zf = zipfile.ZipFile(io.BytesIO(f.read()))
-            os.makedirs(path, exist_ok=True)
-            if file_list is not None:
-                for file in file_list:
-                    zf.extract(file, path)
-            else:
-                zf.extractall(path)
+        try:
+            with urllib.request.urlopen(URL) as f:
+                zf = zipfile.ZipFile(io.BytesIO(f.read()))
+        except OverflowError:
+            with urllib.request.urlopen(URL) as f:
+                CHUNK_SIZE = 1024 * 1024 * 10
+                ftmp = open(path + ".zip", "wb")
+                while True:
+                    data = f.read(CHUNK_SIZE)
+                    ftmp.write(data)
+                    if len(data) == 0:
+                        break
+                ftmp.flush()
+                ftmp.close()
+                zf = zipfile.ZipFile(path + ".zip")
+        
+        os.makedirs(path, exist_ok=True)
+        if file_list is not None:
+            for file in file_list:
+                zf.extract(file, path)
+        else:
+            zf.extractall(path)
+        zf.close()
         return
 
     return DOWNLOAD
