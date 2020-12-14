f4fdc2cc449cedb1ed796138794888c7accabee1
Qianrui Zhou <zhouqianrui@localhost.localdomain>
2020-12-14 14:49:04 +0000

    complete multi-process manner #113, supporting Chinese data #98

--- OpenAttack/text_processors/chinese.py
+++ OpenAttack/text_processors/chinese.py
@@ -31,3 +31,54 @@ class ChineseTextProcessor(TextProcessor):
         for i in range(len(lists)):
             ans.append(tuple(lists[i]))
         return ans
+
+    def get_lemmas(self, token_and_pos):
+        """
+        :param token_and_pos: A tuple or a list of tuples,  *(token, POS)*.
+        :type token_and_pos: list or tuple
+        :return: A lemma or a list of lemmas depends on your input.
+        :rtype: list or str
+        """
+        pass
+    
+    def get_delemmas(self, lemma_and_pos):
+        """
+        :param lemma_and_pos: A tuple or a list of tuples, *(lemma, POS)*.
+        :type lemma_and_pos: list or tuple
+        :return: A word or a list of words, each word represents the specific form of input lemma.
+        :rtype: list or str
+        """
+        pass
+
+    def get_ner(self, sentence):
+        """
+        :param str sentence: A sentence that we want to extract named entities.
+        :return: A list of tuples, *(entity, start, end, label)*
+        :rtype: list
+        """
+        pass
+
+    def get_parser(self, sentence):
+        """
+        :param str sentence: A setence needs to be parsed
+        :return: The result tree of lexicalized parser in string format.
+        :rtype: str
+        """
+        pass
+
+    def get_wsd(self, tokens_and_pos):
+        """
+        :param list tokens_and_pos: A list of tuples, *(token, POS)*.
+        :return: A list of str, represents the sense of each input token.
+        :rtype: list
+        """
+        pass
+
+    def detokenizer(self, tokens):
+        """
+        :param list tokens: A list of tokens
+        :return: A detokenized sentence.
+        :rtype: str
+
+        """
+        return ' '.join(tokens)
