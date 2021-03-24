13ffcb4316b3ff8fee4b62737808687984f61d47
zhang <zhang-tj18@mails.tsinghua.edu.cn>
2021-03-24 15:59:36 +0800

    fix transformer version bug

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ VERSION = "test"
 with open("OpenAttack/version.py", "r") as fver:
     VERSION = fver.read().replace("VERSION", "").replace("=", "").replace("\"", "").strip()
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 requirements = []
