505126aa36c9d14093f1f1091dfe0a09d8183dcc
a710128 <qbjooo@qq.com>
2021-04-15 16:10:04 +0000

    remove debug code

--- OpenAttack/attack_evals/multi_process.py
+++ OpenAttack/attack_evals/multi_process.py
@@ -5,21 +5,6 @@ logger = logging.getLogger(__name__)
 
 # TODO: not tested on tf yet
 
-import code, traceback, signal
-
-def debug(sig, frame):
-    """Interrupt running process, and provide a python prompt for
-    interactive debugging."""
-    d={'_frame':frame}         # Allow access to frame object.
-    d.update(frame.f_globals)  # Unless shadowed by global
-    d.update(frame.f_locals)
-
-    message  = "Signal received : entering python shell.\nTraceback:\n"
-    message += ''.join(traceback.format_stack(frame))
-    print(message)
-
-
-
 def worker(data):
     attacker = globals()["$WORKER_ATTACKER"]
     classifier = globals()["$WORKER_CLASSIFIER"]
--- slow_tests/attackers.py
+++ slow_tests/attackers.py
@@ -9,24 +9,23 @@ def get_attackers(dataset, clsf):
 
     # rules = OpenAttack.attackers.SEAAttacker.get_rules(clsf, dataset)
     print(clsf.embedding.shape)
-    # triggers = OpenAttack.attackers.UATAttacker.get_triggers(clsf, dataset, word2id=clsf.word2id, embedding=clsf.embedding)
-    triggers = ['lame', 'L928', 'ToolsRSSReport']
+    triggers = OpenAttack.attackers.UATAttacker.get_triggers(clsf, dataset, word2id=clsf.word2id, embedding=clsf.embedding)
     print(triggers)
 
     attackers = [
-        # OpenAttack.attackers.FDAttacker(word2id=clsf.word2id, embedding=clsf.embedding, token_unk="[UNK]"),
+        OpenAttack.attackers.FDAttacker(word2id=clsf.word2id, embedding=clsf.embedding, token_unk="[UNK]"),
         # OpenAttack.attackers.SEAAttacker(rules=rules),
-        # OpenAttack.attackers.UATAttacker(triggers=triggers),
-        # OpenAttack.attackers.TextBuggerAttacker(),
-        # OpenAttack.attackers.TextFoolerAttacker(),
-        # OpenAttack.attackers.VIPERAttacker(),
-        # OpenAttack.attackers.DeepWordBugAttacker(),
-        # OpenAttack.attackers.GANAttacker(),
-        # OpenAttack.attackers.GeneticAttacker(),
-        # OpenAttack.attackers.HotFlipAttacker(),
-        # OpenAttack.attackers.PWWSAttacker(),
-        # OpenAttack.attackers.SCPNAttacker(),
-        # OpenAttack.attackers.PSOAttacker(),
+        OpenAttack.attackers.UATAttacker(triggers=triggers),
+        OpenAttack.attackers.TextBuggerAttacker(),
+        OpenAttack.attackers.TextFoolerAttacker(),
+        OpenAttack.attackers.VIPERAttacker(),
+        OpenAttack.attackers.DeepWordBugAttacker(),
+        OpenAttack.attackers.GANAttacker(),
+        OpenAttack.attackers.GeneticAttacker(),
+        OpenAttack.attackers.HotFlipAttacker(),
+        OpenAttack.attackers.PWWSAttacker(),
+        OpenAttack.attackers.SCPNAttacker(),
+        OpenAttack.attackers.PSOAttacker(),
         OpenAttack.attackers.BAEAttacker(),
         OpenAttack.attackers.BERTAttacker()
     ]
