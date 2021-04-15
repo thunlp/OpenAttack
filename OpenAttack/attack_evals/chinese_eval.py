afa8a16e049f3cf011729846d4afbd987ec24dc2
zhang <zhang-tj18@mails.tsinghua.edu.cn>
2021-04-16 00:07:22 +0800

    change text-processor to chinese

--- OpenAttack/attack_evals/chinese_eval.py
+++ OpenAttack/attack_evals/chinese_eval.py
@@ -14,28 +14,16 @@ class ChineseAttackEval(DefaultAttackEval):
         :param Classifier classifier: The classifier you want to attack.
         :param kwargs: Other parameters, see :py:class:`.DefaultAttackEval` for detail.
         """
+        if kwargs["processor"] is None:
+            from ..text_processors import ChineseTextProcessor
+            kwargs["processor"] = ChineseTextProcessor()
         if kwargs["language_model"] is None:
             from ..metric import GPT2LMCH
             kwargs["language_model"] = GPT2LMCH()
-            self.lm = GPT2LMCH()
         if kwargs["language_tool"] is None:
             from ..metric import ChineseLanguageTool
             kwargs["language_tool"] = ChineseLanguageTool()
-            self.lt = ChineseLanguageTool()
         super().__init__(attacker, classifier, **kwargs)
 
         self.__attacker = self.attacker
         self.__classifier = self.classifier
-
-    def get_tokens(self, sent):
-        import thulac
-        tl = thulac.thulac(seg_only=True)
-        return list(t[0] for t in tl.cut(sent))
-
-    def get_fluency(self, sent):
-        if len(sent.strip()) == 0:
-            return 1
-        return self.lm(sent)
-
-    def get_mistakes(self, sent):
-        return len(self.lt(sent))
