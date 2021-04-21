d80e2d6f4cdfbd8300048a01e8bef81afeb426a6
Qianrui Zhou <zhouqianrui@localhost.localdomain>
2021-04-21 04:02:18 +0000

    fix huggingface_classifier

--- OpenAttack/data/victim_albert_ag.py
+++ OpenAttack/data/victim_albert_ag.py
@@ -8,7 +8,7 @@
 Pretrained ALBERT model on AG-4 dataset. See :py:data:`Dataset.AG` for detail.
 """
 
-from OpenAttack.utils import make_zip_downloader, AlbertClassifier
+from OpenAttack.utils import make_zip_downloader
 
 NAME = "Victim.ALBERT.AG"
 
@@ -16,5 +16,8 @@ URL = "https://cdn.data.thunlp.org/TAADToolbox/victim/albert_ag.zip"
 DOWNLOAD = make_zip_downloader(URL)
 
 def LOAD(path):
-    from OpenAttack import Classifier
-    return AlbertClassifier(path, 5)
+    from OpenAttack import HuggingfaceClassifier
+    import transformers
+    tokenizer = transformers.AutoTokenizer.from_pretrained(path)
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(path, num_labels=5, output_hidden_states=False)
+    return HuggingfaceClassifier(model, tokenizer=tokenizer, max_len=100, embedding_layer=model.albert.embeddings.word_embeddings)
--- OpenAttack/data/victim_bert_mnli.py
+++ OpenAttack/data/victim_bert_mnli.py
@@ -8,7 +8,7 @@
 Pretrained BERT model on MNLI dataset. See :py:data:`Dataset.MNLI` for detail.
 """
 
-from OpenAttack.utils import make_zip_downloader, BertClassifier
+from OpenAttack.utils import make_zip_downloader
 
 NAME = "Victim.BERT.MNLI"
 
@@ -16,5 +16,8 @@ URL = "https://cdn.data.thunlp.org/TAADToolbox/victim/bert_mnli.zip"
 DOWNLOAD = make_zip_downloader(URL)
 
 def LOAD(path):
-    from OpenAttack import Classifier
-    return BertClassifier(path, 2)
+    from OpenAttack import HuggingfaceClassifier
+    import transformers
+    tokenizer = transformers.AutoTokenizer.from_pretrained(path)
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(path, num_labels=2, output_hidden_states=False)
+    return HuggingfaceClassifier(model, tokenizer=tokenizer, max_len=100, embedding_layer=model.bert.embeddings.word_embeddings)
