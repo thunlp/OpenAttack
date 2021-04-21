55c6b2975f5981ba0e376bfe1a70ce4acc7b59e8
ztj <58116272+Cypredar@users.noreply.github.com>
2021-04-21 16:04:51 +0800

    Update gptlmch.py
    update chinese language model

--- OpenAttack/metric/gptlmch.py
+++ OpenAttack/metric/gptlmch.py
@@ -18,10 +18,7 @@ class GPT2LMCH:
         self.use_tf = use_tf
 
         self.tokenizer = transformers.BertTokenizer.from_pretrained("mymusise/EasternFantasyNoval")
-        if use_tf:
-            self.lm = transformers.TFGPT2LMHeadModel.from_pretrained("mymusise/EasternFantasyNoval")
-        else:
-            self.lm = transformers.GPT2LMHeadModel.from_pretrained("mymusise/EasternFantasyNoval")
+        self.lm = transformers.TFGPT2LMHeadModel.from_pretrained("mymusise/EasternFantasyNoval")
 
     def __call__(self, sent):
         """
@@ -29,21 +26,15 @@ class GPT2LMCH:
         :return: Fluency (ppl).
         :rtype: float
         """
-        if self.use_tf:
-            import tensorflow as tf
-            ipt = self.tokenizer(sent, return_tensors="tf", verbose=False)
-            ret = self.lm(ipt)[0]
-            loss = 0
-            for i in range(ret.shape[0]):
-                it = ret[i]
-                it = it - tf.reduce_max(it, axis=1)[:, tf.newaxis]
-                it = it - tf.math.log(tf.reduce_sum(tf.exp(it), axis=1))[:, tf.newaxis]
-                it = tf.gather_nd(it, list(zip(range(it.shape[0] - 1), ipt.input_ids[i].numpy().tolist()[1:])))
-                loss += tf.reduce_mean(it)
-                break
-            return math.exp(-loss)
-        else:
-            ipt = self.tokenizer(sent, return_tensors="pt", verbose=False)
-            return math.exp(self.lm(**ipt, labels=ipt.input_ids)[0])
-
-
+        import tensorflow as tf
+        ipt = self.tokenizer(sent, return_tensors="tf", verbose=False)
+        ret = self.lm(ipt)[0]
+        loss = 0
+        for i in range(ret.shape[0]):
+            it = ret[i]
+            it = it - tf.reduce_max(it, axis=1)[:, tf.newaxis]
+            it = it - tf.math.log(tf.reduce_sum(tf.exp(it), axis=1))[:, tf.newaxis]
+            it = tf.gather_nd(it, list(zip(range(it.shape[0] - 1), ipt.input_ids[i].numpy().tolist()[1:])))
+            loss += tf.reduce_mean(it)
+            break
+        return math.exp(-loss)
